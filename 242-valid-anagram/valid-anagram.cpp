81ca8357ad8b7941f7fb7043c0c502a34d90205a
rithviknagamalla <rithviknagamalla@gmail.com>
2026-03-13 22:21:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.14%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    
+     if(s.size()!=t.size()){
+        return false;
+     }
+     int countS[26]={0},countT[26]={0};
+     for(int i=0;i<s.size();i++){
+      countS[s[i]-'a']++;
+      countT[t[i]-'a']++;
+     }
+
+     for(int i=0;i<26;i++){
+        if(countS[i]!=countT[i]){
+            return false;
+        }
+     }
+     return true;
+
+    }
+};
