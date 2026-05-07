3d5ac56eadb9b46db0abd873630467803bf5c5c0
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-07 22:30:34 +0530

    Time: 22 ms (89.43%) | Memory: 71.1 MB (80.24%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+    //   unordered_map<int,int>count;
+
+    //   for(int i=0;i<nums.size();i++){
+       
+    //    count[nums[i]]++;
+    //   }
+    //    for(int i=0;i<nums.size();i++){
+    //     if(count[nums[i]]>1){
+    //         return true;
+    //     }
+    //    }
+    //    return false;
+
+
+          sort(nums.begin(), nums.end());
+    int found=0;
+    for(int i=0;i<nums.size()-1;i++){
+        if(nums[i]==nums[i+1]){
+            found=1;
+        }
+    }    
+    if(found==1){
+        return true;
+    }
+    else{
+ return false;
+    }
+    }
+};
