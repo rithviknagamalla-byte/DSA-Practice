b0c24aaf5fbc1b1a56ee4005418691101d87b2cd
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-07 22:44:25 +0530

    Time: 63 ms (19.64%) | Memory: 92.9 MB (12.67%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -15,18 +15,30 @@ public:
     //    return false;
 
 
-          sort(nums.begin(), nums.end());
-    int found=0;
-    for(int i=0;i<nums.size()-1;i++){
-        if(nums[i]==nums[i+1]){
-            found=1;
-        }
-    }    
-    if(found==1){
+
+    // sort(nums.begin(), nums.end());
+    // int found=0;
+    // for(int i=0;i<nums.size()-1;i++){
+    //     if(nums[i]==nums[i+1]){
+    //         found=1;
+    //     }
+    // }    
+    // if(found==1){
+    // return true;
+    // }
+    // else{
+    // return false;
+    // }
+    unordered_map<int,int>count;
+    for(int i=0;i<nums.size();i++){
+        count[nums[i]]++;
+    }
+    for(int i=0;i<nums.size();i++){
+ if(count[nums[i]]>1){
         return true;
     }
-    else{
- return false;
+
     }
+        return false;
     }
 };
