9714daff53f20071875c20bb7df88d9e5d6069e8
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-07 23:16:15 +0530

    Time: 6 ms (64.31%) | Memory: 14.9 MB (37.08%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -27,5 +27,17 @@ public:
         }
     }
     return -1;
+
+    int freq[26]={0};
+    
+    for(char ch:s){
+      freq[ch-'a']++;
+    }
+   for(int i=0;i<s.size();i++){
+    if(freq[s[i]]==1){
+        return i;
+    }
+   }
+   return -1;
     }
 };
