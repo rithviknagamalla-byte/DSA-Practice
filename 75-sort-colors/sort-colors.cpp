41ad7c7c2e4b2f97bb184bbd1ef7af647b8cf4c0
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-10 18:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (46.94%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -3,40 +3,62 @@ public:
     void sortColors(vector<int>& nums) {
     
     // brute force approach
-    int i=0,j;
-    for(int i=0;i<nums.size();i++){
-        for(int j=i+1;j<nums.size();j++){
-        if(nums[i]>nums[j]){
-            swap(nums[i],nums[j]);
-        }
-        }
-    }
+    // int i=0,j;
+    // for(int i=0;i<nums.size();i++){
+    //     for(int j=i+1;j<nums.size();j++){
+    //     if(nums[i]>nums[j]){
+    //         swap(nums[i],nums[j]);
+    //     }
+    //     }
+    // }
    
    // optimal method
     
-    int low=0,mid=0,high=nums.size()-1;
-    while(mid<high){
-        if(nums[mid]==2){
-            swap(nums[mid],nums[high]);
-            mid++;
-            high--;
+    // int low=0,mid=0,high=nums.size()-1;
+    // while(mid<high){
+    //     if(nums[mid]==2){
+    //         swap(nums[mid],nums[high]);
+    //         mid++;
+    //         high--;
+    //     }
+    //     if(nums[mid]==0){
+    //         swap(nums[low],nums[mid]);
+    //         low=low+1;
+    //         mid=mid+1;
+            
+    //     }
+    //     else{
+    //         mid++;
+    //     }
+
+    // }
+
+     // counting sort
+
+     int zeroes=0,ones=0,twos=0;
+     for(int num:nums){
+        if(num==0){
+            zeroes++;
         }
-        if(nums[mid]==0){
-            low=low+1;
-            mid=mid+1;
+       else if(num==1){
+            ones++;
         }
         else{
-            mid++;
+            twos++;
         }
-
+     }
+    int i=0;
+    while(zeroes--){
+        nums[i++]=0;
+    }
+    
+     while(ones--){
+        nums[i++]=1;
     }
 
-
-
-
-
-
-
+     while(twos--){
+        nums[i++]=2;
+    }
 
 
   }
