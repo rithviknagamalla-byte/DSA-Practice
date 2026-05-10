44427a23e2d8e571cfa1151ec1330dffe7ed036c
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-10 18:20:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (46.94%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-   
+    
+    // brute force approach
     int i=0,j;
     for(int i=0;i<nums.size();i++){
         for(int j=i+1;j<nums.size();j++){
@@ -10,7 +11,13 @@ public:
         }
         }
     }
+   
+   // optimal method
 
+
+
+
+   
 
 
 
