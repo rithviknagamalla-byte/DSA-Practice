6ffec982dbad8f26c6973fac9797a44dc0014b80
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-10 18:09:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (77.55%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+   
+    int i=0,j;
+    for(int i=0;i<nums.size();i++){
+        for(int j=i+1;j<nums.size();j++){
+        if(nums[i]>nums[j]){
+            swap(nums[i],nums[j]);
+        }
+        }
+    }
+
+
+
+
+
+
+  }
+};
