1fb8be34e3d05f98c3cf848e803ca715decc6d60
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-12 17:41:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.41%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,13 +1,40 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+      // linear search 
+    // for(int i=0;i<nums.size();i++){
+    //     if(nums[i]==target){
+    //         return i;
+    //     }
+    // }
+    // binary search
 
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]==target){
-            return i;
+    int low=0;
+    int high=nums.size()-1;
+    int mid=low+(high-low)/2;
+     while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(nums[low]<=nums[mid]){
+           if(target>=nums[low] && target<nums[mid]){
+            high=mid-1;
+            }
+        else{
+            low=mid+1;
         }
     }
-    
+       else{
+        if(target>nums[mid] && target<=nums[high]){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+       }
+
+     }
 
     return -1;
     }
