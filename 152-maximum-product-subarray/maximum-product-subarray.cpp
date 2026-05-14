52fdcca3bfe7b2c2162282162f7a0e5a8bf66c88
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-14 14:28:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (85.76%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,16 +1,36 @@
 class Solution { 
 public:
     int maxProduct(vector<int>& nums) {
-        int i,j;
-         long long maxproduct=nums[0],product;
-        for(i=0;i<nums.size();i++){
-            product=1;
-         for(j=i;j<nums.size();j++){
-            product=product*nums[j];
-            maxproduct=max(maxproduct,product);
-         }
-         
-        }
-        return maxproduct;
+       
+       // bruteforce approach
+    //   int product;
+    //   int maxproduct=nums[0];
+    //     for(int i=0;i<nums.size();i++){
+    //        product=1;
+    //          for(int j=i;j<nums.size();j++){
+    //     product=product*nums[j];
+    //    maxproduct=max(maxproduct,product);
+    //    }
+    // }
+    //   return maxproduct;
+
+     // 2nd approach
+     int product1=1,product2=1;
+     int maxproduct=nums[0];
+     int n =nums.size();
+     for(int i=0;i<nums.size();i++){
+        
+        product1=product1*nums[i];
+        product2=product2*nums[n-i-1];
+       
+         maxproduct=max(maxproduct,max(product1,product2));
+          if(product1==0){
+            product1=1;
+          }
+          if(product2==0){
+            product2=1;
+          }
+     }
+    return maxproduct;
     }
 };
