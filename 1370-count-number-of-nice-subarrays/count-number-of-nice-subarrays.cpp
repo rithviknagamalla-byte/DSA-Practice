63d6077bb33b6f17be6cecd6fcf8f28f8f572c5f
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-21 20:01:36 +0530

    Time: 14 ms (34.22%) | Memory: 77.9 MB (25.41%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+       int atmost(vector<int>nums,int k){
+     int j=0;
+   int oddcounts=0;
+   int ans=0;
+
+   for(int i=0;i<nums.size();i++){
+   
+    if(nums[i]%2!=0){
+        oddcounts=oddcounts+1;
+    }
+
+    while(oddcounts>k){
+        
+        if(nums[j]%2!=0){
+            oddcounts--;
+        }
+       j++;
+    }
+    ans=ans+(i-j+1);
+
+   }
+return ans;
+}
     int numberOfSubarrays(vector<int>& nums, int k) {
     
     // brute force apprroach
@@ -20,19 +43,30 @@ public:
     
    // using hashing
 
-   unordered_map<int,int>mp;
-   int prefixsum=0;
-   int ans=0;
-   mp[0]=1;
-   for(int i=0;i<nums.size();i++){
-    if(nums[i]%2!=0){
-        prefixsum=prefixsum+1;
-    }
-    if(mp.find(prefixsum-k)!=mp.end()){
-        ans=ans+mp[prefixsum-k];
-    }
-    mp[prefixsum]++;
-   }
-   return ans;
+//    unordered_map<int,int>mp;
+//    int prefixsum=0;
+//    int ans=0;
+//    mp[0]=1;
+//    for(int i=0;i<nums.size();i++){
+//     if(nums[i]%2!=0){
+//         prefixsum=prefixsum+1;
+//     }
+//     if(mp.find(prefixsum-k)!=mp.end()){
+//         ans=ans+mp[prefixsum-k];
+//     }
+//     mp[prefixsum]++;
+//    }
+//    return ans;
+
+   // 2pointers 
+
+  return atmost(nums,k)-atmost(nums,k-1);
     }
 };
+
+
+
+
+
+
+
