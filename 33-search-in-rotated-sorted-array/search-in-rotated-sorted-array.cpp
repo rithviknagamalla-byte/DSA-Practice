7a9577be7b6bda0118fd502c53998cea1fa33b11
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-22 12:59:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.43%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-      // linear search 
-    // for(int i=0;i<nums.size();i++){
-    //     if(nums[i]==target){
-    //         return i;
-    //     }
-    // }
-    // binary search
+    
 
-    int low=0;
-    int high=nums.size()-1;
+    int low=0,high=nums.size()-1;
+
+    while(low<=high){
     int mid=low+(high-low)/2;
-     while(low<=high){
-        int mid=low+(high-low)/2;
-        if(nums[mid]==target){
-            return mid;
-        }
-        if(nums[low]<=nums[mid]){
-           if(target>=nums[low] && target<nums[mid]){
+
+    if(nums[mid]==target){
+        return mid;
+     }
+
+    //left sorted
+    if(nums[low]<=nums[mid]){
+        if(nums[low]<=target && target<nums[mid]){
             high=mid-1;
-            }
+        }
         else{
             low=mid+1;
         }
     }
-       else{
-        if(target>nums[mid] && target<=nums[high]){
-            low=mid+1;
-        }
-        else{
-            high=mid-1;
-        }
-       }
 
+    // right sorted
+    else{
+     if(target>nums[mid] && target<=nums[high]){
+        low=mid+1;
      }
+    
+    else{
+        high=mid-1;
+    }
+    }
 
+    }
     return -1;
     }
 };
