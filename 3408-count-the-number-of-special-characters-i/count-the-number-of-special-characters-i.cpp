c84e60b944b6e877e3f711a5fbf6d8e298db452a
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-26 10:30:31 +0530

    Time: 1 ms (39.91%) | Memory: 9.3 MB (40.14%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -2,24 +2,43 @@ class Solution {
 public:
     int numberOfSpecialChars(string word) {
 
-      set<char>lower;
-      set<char>upper;
+    //   set<char>lower;
+    //   set<char>upper;
 
-      for(char ch:word){
-        if(ch>='a' && ch<='z'){
-       lower.insert(ch);
+    //   for(char ch:word){
+    //     if(ch>='a' && ch<='z'){
+    //    lower.insert(ch);
+    //     }
+    //     else if(ch>='A' && ch<='Z'){
+    //      upper.insert(ch);
+    //     }
+    //   }
+    //    int count=0;
+    //    for(char ch:lower){
+    //     if(upper.count(ch-32)){
+    //         count++;
+    //     }
+    //    }
+
+    //  return count;
+
+     vector<int>lower(26,0);
+     vector<int>upper(26,0);
+
+     for(char ch:word){
+        if(ch>='a'&& ch<='z'){
+            lower[ch-'a']=1;
         }
-        else if(ch>='A' && ch<='Z'){
-         upper.insert(ch);
+        else if(ch>='A'&& ch<='Z'){
+            upper[ch-'A']=1; 
         }
-      }
-       int count=0;
-       for(char ch:lower){
-        if(upper.count(ch-32)){
+     }
+     int count=0;
+    for(int i=0;i<26;i++){
+        if(lower[i]&&upper[i]==1){
             count++;
         }
-       }
-
+    }
      return count;
     }
 };
