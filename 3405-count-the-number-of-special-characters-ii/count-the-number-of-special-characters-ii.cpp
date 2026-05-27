c9e2bcc47cb3fbe4250d7f680a367b51b2615da5
rithviknagamalla <rithviknagamalla@gmail.com>
2026-05-27 16:12:04 +0530

    Time: 87 ms (30.34%) | Memory: 25.7 MB (23.31%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -2,25 +2,53 @@ class Solution {
 public:
     int numberOfSpecialChars(string word) {
         
-    vector<int>lower(26,-1);
-    vector<int>upper(26,-1);
+    // vector<int>lower(26,-1);
+    // vector<int>upper(26,-1);
 
-    for(int i=0;i<word.size();i++){
-        if(word[i]>='a' && word[i]<='z'){
-            lower[word[i]-'a']=i;
-        }
-        else if(word[i]>='A' && word[i]<='Z'){
-           if(upper[word[i]-'A']==-1){
-            upper[word[i]-'A']=i;
-           }
-        }
+    // for(int i=0;i<word.size();i++){
+    //     if(word[i]>='a' && word[i]<='z'){
+    //         lower[word[i]-'a']=i;
+    //     }
+    //     else if(word[i]>='A' && word[i]<='Z'){
+    //        if(upper[word[i]-'A']==-1){
+    //         upper[word[i]-'A']=i;
+    //        }
+    //     }
+    // }
+    // int count=0;
+    // for(int i=0;i<26;i++){
+    //  if(lower[i]!=-1 && upper[i]!=-1 && lower[i]<upper[i]){
+    //     count++;
+    //  }
+    // }
+    // return count;
+    
+   unordered_map<char,int>lower;
+   unordered_map<char,int>upper;
+   
+   for(int i=0;i<word.size();i++){
+    char ch=word[i];
+    if(ch>='a' && ch<='z'){
+        lower[ch]=i;
     }
-    int count=0;
-    for(int i=0;i<26;i++){
-     if(lower[i]!=-1 && upper[i]!=-1 && lower[i]<upper[i]){
+    else if(ch>='A' && ch<='Z'){
+        if(upper.find(ch)==upper.end()){
+            upper[ch]=i;
+        }
+
+    }
+   }
+  
+  int count=0;
+
+  for(char ch='a';ch<='z';ch++){
+    char chair=ch-32;
+    if(lower.find(ch)!=lower.end() && upper.find(chair)!=upper.end() && lower[ch]<upper[chair]){
         count++;
-     }
     }
-    return count;
+  }
+  
+  return count;
+
     }
 };
