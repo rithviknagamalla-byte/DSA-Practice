e35725820ca2fd5c32d2d70b81754f8ba4756011
rithviknagamalla <rithviknagamalla@gmail.com>
2026-07-02 22:26:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (6.69%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-
-      unordered_map<char,int>mp;
+         unordered_map<char,int>mp;
       for(char ch:text){
         mp[ch]++;
       }
