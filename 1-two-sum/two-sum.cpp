efa3207ebab3916e85ab217decaed41c32d09cc6
rithviknagamalla <rithviknagamalla@gmail.com>
2026-08-07 14:39:12 +0530

    Time: 2 ms (72.06%) | Memory: 15 MB (12.02%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target){
-    // brute force 
-//     for(int i=0;i<nums.size();i++){
-//         for(int j=i+1;j<nums.size();j++){
-//             if(nums[i]+nums[j]==target){
-//                 return {i,j};
-//             }
-//         }
-//     }
-// return { };
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //bruteforce approach
+    //  for(int i=0;i<nums.size();i++){
+    //     for(int j=i+1;j<nums.size();i++){
+    //         if(nums[i]+nums[j]==target){
+    //             return {i,j};
+    //         }
+    //     }
+    //  } 
 
-  //optimal solution
-unordered_map<int,int>mpp;
-
-for(int i=0;i<nums.size();i++){
-    int diff=target-nums[i];
-    if(mpp.count(diff)){
-        return {mpp[diff],i};
+    unordered_map<int,int>mp;
+    for(int i=0;i<nums.size();i++){
+     int diff=target-nums[i];
+     if(mp.find(diff)!=mp.end()){
+        return {mp[diff],i};
+     }
+      mp[nums[i]]=i;
     }
-    mpp[nums[i]]=i;
-}
-return { };
 
+     return {};  
     }
 };
