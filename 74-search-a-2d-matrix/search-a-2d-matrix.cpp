1532c9e682dbac96c908ea862cbd8b6df7dd94d9
rithviknagamalla <rithviknagamalla@gmail.com>
2026-08-10 17:40:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (80.02%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+     int m=matrix.size();
+     int n=matrix[0].size();
+   
+   int start=0,end=(m*n)-1;
+   while(start<=end){
+    int mid=(start+end)/2;
+    if(matrix[mid/n][mid%n]==target){
+        return true;
+    }
+    else if(matrix[mid/n][mid%n]>target){
+        end=mid-1;
+    }
+    else{
+        start=mid+1;
+    }
+
+   }
+
+  return false;
+    }
+};
