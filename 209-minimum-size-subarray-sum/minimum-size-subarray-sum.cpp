113e2f4e7ec814bf4a50595fb088baeef6944b4a
rithviknagamalla <rithviknagamalla@gmail.com>
2026-08-12 21:24:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (67.59%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-    // bruteforce
-//    int sum;
-//    long long length=nums.size()+1;
-//    long long minlength=nums.size()+1;
+
+//     int minlength=INT_MAX;
+//     int length=0;
 //     for(int i=0;i<nums.size();i++){
-//         sum=0;
-//       for(int j=i;j<nums.size();j++){
-//          sum=sum+nums[j];
-//          if(sum>=target){
-//          length=j-i+1;
-//          }
-//         minlength=min(minlength,length);  
-//        }  
-//  }
-//     if(minlength==nums.size()+1){
-//     return 0;
+//         int sum=0;
+//         for(int j=i;j<nums.size();j++){
+//           sum = sum+nums[j];
+        
+//         if(sum>=target){
+//             length=j-i+1;
+//            minlength=min(length,minlength);
+//            break;
+//         }
+//         }
 //     }
-//     return minlength;    
-   
-   int sum=0;
-   int minlength=nums.size()+1;
-   int j=0;
-   int length=nums.size()+1;
-    for(int i=0;i<nums.size();i++){
-        sum=sum+nums[i];
-       while(sum>=target){
-        minlength=min(minlength,i-j+1);
-        sum=sum-nums[j];
-        j++;
-       }
-       
+//     if(minlength==INT_MAX){
+//         return 0;
+//     }
+//    return minlength;
+
+int sum=0;
+int j=0;
+int minlength=INT_MAX;
+for(int i=0;i<nums.size();i++){
+     sum=sum+nums[i];
+
+    while(sum>=target){
+    minlength=min(minlength,i-j+1);
+    sum=sum-nums[j];
+    j++;
     }
-    if(minlength==nums.size()+1){
-     return 0;
-    }
+}
+
+if(minlength==INT_MAX){
+    return 0;
+}
+
 return minlength;
-}     
+    }
 };
