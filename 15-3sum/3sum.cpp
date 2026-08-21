10eca19069dc881c26d7a0e4b0336be27bcb5d01
rithviknagamalla <rithviknagamalla@gmail.com>
2026-08-21 10:17:13 +0530

    Time: 51 ms (42.04%) | Memory: 29 MB (92.46%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,39 +2,79 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
 
-    int left=0;
-    vector<vector<int>>ans;
-    sort(nums.begin(),nums.end());
-    while(left<nums.size()-2){
+     int left=0;
+     sort(nums.begin(),nums.end());
+     vector<vector<int>>ans;
+     while(left<nums.size()-2){
      int mid=left+1;
      int right=nums.size()-1;
-    if(left>0 && nums[left]==nums[left-1]){
-            left++;
-            continue;
-        }
-     while(mid<right){
+     if(left>0 && nums[left]==nums[left-1])
+     {
+       left++;
+       continue;
+     }
+
+     while(mid<right)
+    {
         if(nums[left]+nums[mid]+nums[right]==0){
-        ans.push_back({nums[left],nums[mid],nums[right]});
-        mid++;
-        right--;
-        while(mid<right && nums[mid]==nums[mid-1]){
+            ans.push_back({nums[left],nums[mid],nums[right]});
             mid++;
-            continue;
-        }
-        while(mid<right && nums[right]==nums[right+1]){
             right--;
-            continue;
+            while(mid<right && nums[mid]==nums[mid-1]){
+                mid++;
+                continue;
+            }
+            while(mid<right && nums[right]==nums[right+1]){
+                right--;
+                continue;
+            }
         }
-        }
-        else if(nums[left]+nums[mid]+nums[right]<0){
-        mid++;
+        else if(nums[left]+nums[mid]+nums[right]>0){
+            right--;
         }
         else{
-        right--;
-        }
-     }
-     left++;
+            mid++;
     }
+    }
+
+left++;
+     }
+
+
+
+    // int left=0;
+    // vector<vector<int>>ans;
+    // sort(nums.begin(),nums.end());
+    // while(left<nums.size()-2){
+    //  int mid=left+1;
+    //  int right=nums.size()-1;
+    // if(left>0 && nums[left]==nums[left-1]){
+    //         left++;
+    //         continue;
+    //     }
+    //  while(mid<right){
+    //     if(nums[left]+nums[mid]+nums[right]==0){
+    //     ans.push_back({nums[left],nums[mid],nums[right]});
+    //     mid++;
+    //     right--;
+    //     while(mid<right && nums[mid]==nums[mid-1]){
+    //         mid++;
+    //         continue;
+    //     }
+    //     while(mid<right && nums[right]==nums[right+1]){
+    //         right--;
+    //         continue;
+    //     }
+    //     }
+    //     else if(nums[left]+nums[mid]+nums[right]<0){
+    //     mid++;
+    //     }
+    //     else{
+    //     right--;
+    //     }
+    //  }
+    //  left++;
+    // }
     return ans;
     }
 };
