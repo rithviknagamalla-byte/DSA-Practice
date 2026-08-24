5ba20161d20885bae314651020515d1153ec475d
rithviknagamalla <rithviknagamalla@gmail.com>
2026-08-25 00:48:08 +0530

    Time: 10 ms (26.41%) | Memory: 15.3 MB (44.17%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int calculate(string s) {
+        
+     stack<int>st;
+     int num=0;
+     char op='+';
+     for(int i=0;i<s.size();i++){
+        if(isdigit(s[i])){
+        num=num*10+(s[i]-'0');
+        }
+         
+    if((!isdigit(s[i]) && s[i]!=' ')|| i==s.size()-1){
+    if(op=='*'){
+    st.top()=st.top()*num;
+    }
+    else if(op=='/'){
+    st.top()=st.top()/num;
+    }
+    else if(op=='+'){
+    st.push(num);
+    }
+    else{
+    st.push(-num);
+    }
+    op=s[i];
+    num=0;
+    } 
+ }
+    int ans=0;
+    while(!st.empty()){
+        ans=ans+st.top();
+        st.pop();
+    }
+     
+
+     return ans;
+    }
+};
