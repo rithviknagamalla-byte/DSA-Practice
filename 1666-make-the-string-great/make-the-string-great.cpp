818bae319a466907e198750b6566384ad4facdf8
rithviknagamalla <rithviknagamalla@gmail.com>
2026-09-07 23:56:32 +0530

    Time: 2 ms (19.68%) | Memory: 9 MB (9.03%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     string makeGood(string s) {
-     stack<char>st;
-     string ans="";
-     int c1,c2;
-     for(int i=0;i<s.size();i++){
-        if(!st.empty()){
-            c1=st.top();
+    
+    stack<char>st;
+    string ans="";
+    for(int i=0;i<s.size();i++){
+        if(!st.empty() && abs(s[i]-st.top())==32){
+         st.pop();
         }
-        c2=s[i];
-        if(!st.empty() && abs(c1-c2)==32){
-            st.pop(); 
-         }
         else{
-            st.push(s[i]);
-     
-        }  
+         st.push(s[i]);
+        }
+    }
 
-     }   
-     while(!st.empty()){
-        ans = ans+st.top();
-        st.pop();
-     }
-     reverse(ans.begin(),ans.end());
-     return ans;
+    while(!st.empty()){
+    ans=ans+st.top();
+    st.pop();
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
     }
 };
